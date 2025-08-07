4e417abac13fff5ab738537f3def1e32f3de4042
wisehelp <wisehelp22@gmail.com>
2025-08-07 17:32:33 +0900

    같이 던전 오실래요? NYPC round 1

---
+++ Biko/Kotlin/같이 던전 도실래요.kt
@@ -0,0 +1,17 @@
+package Biko.Kotlin
+
+fun main(){
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    val t = readLine()!!.split(" ").map { it.toInt() }.sorted()
+
+    var result: Long = 0
+    for(i in 0 until n){
+        val a = t[2 * i]
+        val b = t[2 * i + 1]
+        val mint = minOf(a, b)
+        val clear = mint / m
+        val s = 2 * mint
+        result += s * clear
+    }
+    println(result)
+}
